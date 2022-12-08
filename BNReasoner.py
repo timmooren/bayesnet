ba2b0842ad1c8a1c5e148a3eb416b39f233e42cc
agnesadm1997 <70509812+agnesadm1997@users.noreply.github.com>
2022-12-08 13:48:26 +0100

    Update BNReasoner.py

--- BNReasoner.py
+++ BNReasoner.py
@@ -7,10 +7,10 @@
 
 from typing import Union, List
 from BayesNet import BayesNet
+from heuristics import min_degree, min_fill
 import pandas as pd
 import networkx as nx
 from copy import deepcopy
-import matplotlib.pyplot as plt
 import pandas as pd
 
 class BNReasoner():
@@ -37,7 +37,7 @@ class BNReasoner():
         self.order_method = order_method
 
 
-# --------------------------------- NETWORK PRUNING --------------------------------- #
+# --------------------------------- INDEPENDENCE DETERMINATION --------------------------------- #
 
     def prune(self, query: List[str], evidence: pd.Series) -> bool:
             """
@@ -75,174 +75,7 @@ class BNReasoner():
                         keep_node_pruning = False
 
             return bn  
-    
-     
-# --------------------------------- HELPER FUNCTIONS --------------------------------- #
-
-    def marginalization(self, factor: pd.DataFrame,  X: str) -> pd.DataFrame:
-        """
-        Given a factor and a variable X, compute the CPT in which X is summed-out.
-        """
-        groups = factor.columns.to_list()
-        groups.remove(X)
-        groups.remove('p')
         
-        return factor.groupby(groups, as_index=False).sum()
-    
-
-    def max_out(self, factor: pd.DataFrame, X: str) -> pd.DataFrame:
-        """
-        Given a factor and a variable X, compute the CPT in which X is maxed-out.
-        Remember to also keep track of which instantiation of X led to the maximized value.
-        """
-        groups = factor.columns.to_list()
-        groups.remove(X)
-        groups.remove('p')
-        
-        return factor.groupby(groups, as_index=False).max()
-
-
-    def factor_multiplication(self, factor1: pd.DataFrame, factor2: pd.DataFrame) -> pd.DataFrame:
-        """
-        Given two factors f and g, compute the multiplied factor h=fg.
-        """
-        # find the common columns of the two factors
-        common_columns = list(set(factor1.columns).intersection(factor2.columns))
-        common_columns.remove('p')
-
-        # merge the two factors by multiplying the probabilities
-        output = factor1.merge(factor2, on=common_columns)   
-        output['p'] = output['p_x'] * output['p_y']
-        output.drop(['p_x', 'p_y'], axis=1, inplace=True)
-    
-        return output
-
-
-    def find_ancestors(self, node : str, ancestors : List[str]) -> List[str]: 
-        """
-        Given a node, find all ancestors of this node
-        """
-        ancestors = ancestors
-        
-        # find parents of node
-        parents = list(self.bn.structure.predecessors(node))
-        
-        # stop when no parents are found
-        if parents:
-            
-            # find all ancestors of parents
-            for parent in parents:
-                ancestors.append(parent)
-                ancestors += self.find_ancestors(parent, ancestors)
-                
-                # remove duplicates
-                ancestors = list(set(ancestors))
-                
-        return ancestors
-    
-    
-    def find_order(self) -> List[str]:
-        """
-        Given a set of variables X in the Bayesian network, 
-        compute a good ordering for the elimination of X
-        """
-        
-        self.order_method = 'fill'                                           # TODO: for some reason does not work as class attribute if this line is left out
-        order = [] # list of nodes in order of elimination
-        interaction_graph = self.bn.get_interaction_graph()
-        
-        # elliminate node by node until all nodes are elliminated
-        for i in range(len(interaction_graph.nodes())-1):
-            
-            # compute number of edges for each node
-            degrees = interaction_graph.degree()
-            
-            # determine wich node to elliminate next                        # TODO: implement other methods? 
-            if self.order_method == 'min':
-                chosen_node, _ = self.min_degree(degrees)
-            if self.order_method == 'fill':
-                chosen_node, _ = self.fill_in(interaction_graph)
-              
-            order.append(chosen_node)
-            
-            # find direct neigbour nodes
-            neighbours = list(interaction_graph.neighbors(chosen_node))
-            other_neighbours = [n for n in neighbours]
-            
-            # create new neighbourhood for each neighbour
-            for neighbour in neighbours:
-                
-                # skip when no other neighbours
-                if len(other_neighbours) > 1:
-    
-                    # prevent neigbour from connecting to itself
-                    other_neighbours.remove(str(neighbour))
-                    
-                    # fill in missing edges between neighbours in neighbourhood
-                    for n in other_neighbours:
-                
-                        if not interaction_graph.has_edge(n, neighbour):
-                            interaction_graph.add_edge(neighbour, n)
-
-            # remove current node and edges
-            interaction_graph.remove_node(chosen_node)     
-        
-        # add last node  
-        order.append(list(interaction_graph.nodes())[0])
-        
-        return order
-   
-   
-    # --------------------------------- BAYES PROBABILITY FUNCTIONS --------------------------------- #
-    
-    def bayes_probability(self, query: List[str], evidence: List[str]) -> pd.DataFrame:
-        """
-        Given a set of variables X in the Bayesian network, 
-        compute the CPT of X given the evidence.
-        """      
-        for node in query:
-        
-            # get ancestors of the query variable
-            ancestors = self.find_ancestors(node, [])
-
-            # get the order of elimination       
-            order = self.find_order()     
-            order = [element for element in order if element in ancestors]
-            
-            # make a list of all cpts in the BN
-            list_tables = list(self.bn.get_all_cpts().values())
-            
-            for ancestor in order:   
-                
-                # get cpts for the variable and its ancestors
-                tables_ancestors = [table for table in list_tables if ancestor in table.columns]
-                tables_copy = tables_ancestors.copy()
-
-                # multiply all cpts of this variable
-                for i in range(len(tables_copy)-1):
-                    product = self.factor_multiplication(tables_ancestors[i], tables_ancestors[i+1])
-                    tables_ancestors[i+1]= product
-                
-                # remove allready multiplied cpts from list
-                order.remove(ancestor)
-
-                # and append summed out table of ancestor to list
-                product = self.marginalization(product, ancestor)
-                list_tables.append(product)
-
-        return product
-    
-    
-    def marginal_distributions(self, query: List[str], evidence: List[str]) -> pd.DataFrame:
-        """
-        Given a set of variables X in the Bayesian network, 
-        compute the marginal distribuition of X given the evidence.
-        """
-        # TODO: implement                                                                                       # kan mss ook in de bayes probability functie
-        return
-
-
-     # --------------------------------- DEPENDANCY DETERMINATION --------------------------------- #
 
     def d_separation(self, X: List[str], Y: List[str], Z: List[str]) -> bool:
         """
@@ -271,51 +104,174 @@ class BNReasoner():
         Given three sets of variables X, Y, and Z, determine whether X is independent of Y given Z
         """
         return self.d_separation(X, Y, Z)
-       
-    # --------------------------------- ORDERING HEURISTICS --------------------------------- #	
+    
+     
+# --------------------------------- MAX OUT --------------------------------- #
 
-    def min_degree(self, degrees: dict)-> str:
+    @staticmethod
+    def max_out(factor: pd.DataFrame, X: str) -> pd.DataFrame:
         """
-        Method 1: min-degree heuristic.
-        Given a dictionary of nodes with their degrees, return the node which should be eliminated next.
+        Given a factor and a variable X, compute the CPT in which X is maxed-out.
+        Remember to also keep track of which instantiation of X led to the maximized value.
         """
-        # get the dnode with the minimum number of neighbours
-        next_node = min(degrees, key=lambda x: x[1]) 
-        return next_node 
-
-
-    def fill_in(self, interaction_graph: nx.DiGraph)-> str:
-        """
-        Method 2: fill-in heuristic.
-        Given an interaction graph, return the node which should be eliminated next.
-        """
-        dict_points = {} # dict to store the number of penalty points for each node
-        interaction_graph = interaction_graph
+        groups = factor.columns.to_list()
+        groups.remove(X)
+        groups.remove('p')
         
-        for node in interaction_graph.nodes():
+        return factor.groupby(groups, as_index=False).max()
+
+
+# --------------------------------- HELPER FUCTIONS BAYES --------------------------------- #
+
+    @staticmethod
+    def marginalization(factor: pd.DataFrame,  variable: str) -> pd.DataFrame:
+        """
+        Given a factor and a variable X, compute the CPT in which X is summed-out.
+        """
+        groups = factor.columns.to_list()
+        groups.remove(variable)
+        groups.remove('p')
+        cpt = factor.groupby(groups, as_index=False).sum()
+        #print(cpt)
+        cpt.drop(variable, axis=1, inplace=True)
+
+        return cpt
+    
+    @staticmethod
+    def factor_multiplication(factor1: pd.DataFrame, factor2: pd.DataFrame) -> pd.DataFrame:
+        """
+        Given two factors f and g, compute the multiplied factor h=fg.
+        """
+        # find the common columns of the two factors
+        common_columns = list(set(factor1.columns).intersection(factor2.columns))
+        common_columns.remove('p')
+
+        # merge the two factors by multiplying the probabilities
+        output = factor1.merge(factor2, on=common_columns)   
+        output['p'] = output['p_x'] * output['p_y']
+        output.drop(['p_x', 'p_y'], axis=1, inplace=True)
+    
+        return output
+
+
+    # --------------------------------- BAYES PROBABILITY FUNCTIONS --------------------------------- #
+    
+    def variable_elimination(self, query: List[str], evidence: pd.Series = pd.Series()) -> pd.DataFrame:
+        """
+        Given a set of variables X in the Bayesian network, 
+        compute the CPT of X given the evidence.
+        """      
+        
+        # get ancestors of the query variable
+        ancestors_sets = [nx.ancestors(self.bn.get_interaction_graph(), node) for node in query]
+        intersection_set = set.union(*ancestors_sets)
+        ancestors = list(intersection_set)
+        print(f"ancestors \n {ancestors}")
+        
+        # remove query from collective ancestors
+        for node in query:
+            ancestors = [ancestor for ancestor in ancestors if not node in ancestors]
+        
+        # get the order of elimination       
+        order = self.find_order(ancestors)     
+        
+        # make a list of all cpts in the BN
+        cpt_list = list(self.bn.get_all_cpts().values())
+        print(f"cpt_list: \n{cpt_list}")
+        
+        # marginalize for every ancestor
+        for ancestor in order: 
             
-            neighbours = list(interaction_graph.neighbors(node))
+            # get cpts for the variable and its ancestors
+            for variable in ancestors:
+                tables_ancestors = [table for table in cpt_list if (ancestor in table.columns and variable in table.columns)]
+            tables_copy = tables_ancestors.copy()
+
+            # multiply all cpts for every cpt
+            for i in range(len(tables_copy)-1):
+                product = self.factor_multiplication(tables_ancestors[i], tables_ancestors[i+1])
+                print(f"multiply: \n {tables_ancestors[i]}")
+                print(f"multiply: \n {tables_ancestors[i+1]}")
+                print(f"product: \n{product}")
+                tables_ancestors[i+1]= product
+            
+            # remove all instantiations that are incompatible with the evidence
+            if not evidence.empty:
+                self.bn.get_compatible_instantiations_table(evidence, product)  
+            
+            # remove already multiplied cpts from list
+            cpt_list = [table for table in cpt_list if ancestor not in table.columns]
+
+            # and append summed out table of ancestor to list
+            product = self.marginalization(product, ancestor)
+            print(f"marginalized:\n {product}")
+            cpt_list.append(product)
+            
+            #print("DONE WITH LOOP")
+
+        return product
+    
+    
+    def marginal_distributions(self, query: List[str], evidence: pd.Series) -> pd.DataFrame:
+        """ Sum out a set of variables by using variable elimination."""
+        prior_marginal = self.variable_elimination(query, evidence)
+        evidence_cpt = self.bn.get_cpt(evidence.index)
+        print(evidence_cpt)
+        prior_prob_evidence = evidence_cpt[evidence.index == evidence.values]
+
+        return prior_marginal
+         
+
+# --------------------------------- ORDER DETERMINATION BAYES -------------------------------- #
+    
+    
+    def find_order(self, variables = List[str]) -> List[str]:
+        """
+        Given a set of variables X in the Bayesian network, 
+        compute a good ordering for the elimination of X
+        """
+        
+        self.order_method = 'min'                                           # TODO: for some reason does not work as class attribute if this line is left out
+        order = [] # list of nodes in order of elimination
+        interaction_graph = self.bn.get_interaction_graph()     
+        
+        variables_copy = variables.copy()
+        
+        # elliminate node by node until all nodes are eliminated
+        for node in variables_copy:
+            
+            # determine wich node to elliminate next                        # TODO: implement other methods? 
+            if self.order_method == 'min':
+                chosen_node = min_degree(interaction_graph, variables)
+                
+            if self.order_method == 'fill':
+                chosen_node = min_fill(interaction_graph, variables)
+            
+            order.append(chosen_node)  
+            variables.remove(chosen_node)         
+            
+            # find direct neigbour nodes
+            neighbours = list(interaction_graph.neighbors(chosen_node))
             other_neighbours = [n for n in neighbours]
             
-            dict_points[node] = 0
-            
-            # connect neighbours with edges
+            # create new neighbourhood for each neighbour
             for neighbour in neighbours:
                 
+                # skip when no other neighbours
                 if len(other_neighbours) > 1:
-
-                    other_neighbours.remove(neighbour)
+    
+                    # prevent neigbour from connecting to itself
+                    other_neighbours.remove(str(neighbour))
                     
-                    # if neighbour has no connection to one of other neighbours, add edge
+                    # fill in missing edges between neighbours in neighbourhood
                     for n in other_neighbours:
-                        
+                
                         if not interaction_graph.has_edge(n, neighbour):
-                            dict_points[node] += 1
+                            interaction_graph.add_edge(neighbour, n)
+
+            # remove current node and edges
+            interaction_graph.remove_node(chosen_node)     
         
-        # find node with minimum penalty points
-        elimination_order = min(dict_points.items(), key=lambda x: x[1])
-     
-        return elimination_order
-    
-    
-    
+        return order
+   
+       
